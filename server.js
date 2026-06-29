3c950a1d8ad43a0c003ab2386021a2025ef8e033
rishabhyadavtech <rishabh.yadavtech1@gmail.com>
2026-06-29 09:17:59 +0530

    Add safe browsing check to server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const { decideAction } = require("./logic/decisionHelper");
 const { checkUrlReputation } = require("./logic/urlReputation");
 const { analyzeURL } = require("./logic/urlAnalyzer");
 const { checkDomainAge } = require("./logic/domainAge");
+const { checkSafeBrowsing } = require("./logic/safeBrowsing");
 
 app.use(express.json());
 app.use(express.static("public"));
