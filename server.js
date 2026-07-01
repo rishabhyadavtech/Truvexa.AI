5b3d9b7f0d294543fdd07fbec2ca32c1f0ada617
rishabhyadavtech <rishabh.yadavtech1@gmail.com>
2026-07-01 11:41:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -239,6 +239,9 @@ app.post("/debug", (req, res) => {
 // PORT
 const PORT = process.env.PORT || 3000;
 
+console.log("Safe Browsing Key:",
+process.env.GOOGLE_SAFE_BROWSING_API_KEY ? "Loaded ✅" : "Missing ❌");
+
 app.listen(PORT, () => {
   console.log("🚀 Truvexa running on port " + PORT);
 });
