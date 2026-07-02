a9849a41b8174e2a94386af6dcdd2cfaac20bd21
rishabhyadavtech <rishabh.yadavtech1@gmail.com>
2026-07-02 10:25:05 +0530

    Update virusTotal.js

--- logic/virusTotal.js
+++ logic/virusTotal.js
@@ -25,6 +25,80 @@ async function checkVirusTotal(url) {
     };
   }
 
+try {
+
+  // Step 1 - Submit URL
+  const submit = await fetch(VIRUSTOTAL_API, {
+    method: "POST",
+    headers: {
+      "x-apikey": process.env.VIRUSTOTAL_API_KEY,
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: `url=${encodeURIComponent(url)}`
+  });
+
+  const submitData = await submit.json();
+
+  if (!submit.ok) {
+    throw new Error("Submit failed");
+  }
+
+  const analysisId = submitData.data.id;
+
+  // Step 2 - Get Report
+  const report = await fetch(
+    `https://www.virustotal.com/api/v3/analyses/${analysisId}`,
+    {
+      headers: {
+        "x-apikey": process.env.VIRUSTOTAL_API_KEY
+      }
+    }
+  );
+
+  const reportData = await report.json();
+
+  const stats =
+    reportData.data.attributes.stats;
+
+  return {
+
+    success: true,
+
+    safe: stats.malicious === 0,
+
+    malicious: stats.malicious,
+
+    suspicious: stats.suspicious,
+
+    harmless: stats.harmless,
+
+    undetected: stats.undetected,
+
+    message: "VirusTotal analysis completed."
+
+  };
+
+} catch (err) {
+
+  return {
+
+    success: false,
+
+    safe: true,
+
+    malicious: 0,
+
+    suspicious: 0,
+
+    harmless: 0,
+
+    undetected: 0,
+
+    message: "VirusTotal API unavailable."
+
+  };
+
+}
   
 module.exports = {
   checkVirusTotal
