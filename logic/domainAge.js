d8281df86be6aead27772c1f0c9f9fab6a52475b
rishabhyadavtech <rishabh.yadavtech1@gmail.com>
2026-07-09 15:15:14 +0530

    Update domainAge.js

--- logic/domainAge.js
+++ logic/domainAge.js
@@ -1,6 +1,24 @@
 const { URL } = require("url");
 require("dotenv").config();
 
+function extractDomain(input) {
+  try {
+    let url = input.trim();
+
+    if (
+      !url.startsWith("http://") &&
+      !url.startsWith("https://")
+    ) {
+      url = "https://" + url;
+    }
+
+    return new URL(url).hostname.toLowerCase();
+
+  } catch {
+    return null;
+  }
+}
+
 async function checkDomainAge(input) {
 
   const domain = extractDomain(input);
@@ -98,3 +116,7 @@ async function checkDomainAge(input) {
   }
 
 }
+
+module.exports = {
+  checkDomainAge
+};
