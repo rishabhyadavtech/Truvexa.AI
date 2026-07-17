313a8afbf435fe636df8acd417cfe0cebfa75f12
rishabhyadavtech <rishabh.yadavtech1@gmail.com>
2026-07-17 06:04:20 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -372,6 +372,24 @@ confidenceCard.style.display = "none";
   }  
 }  
 
+function toggleDetail(id){
+
+const box=document.getElementById(id);
+
+if(box.style.display==="none"){
+
+box.style.display="block";
+
+}
+
+else{
+
+box.style.display="none";
+
+}
+
+}
+
 
 // =========================
 // 👍 FEEDBACK SYSTEM
