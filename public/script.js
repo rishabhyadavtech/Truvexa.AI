c381345a80a83b6a0d3c6c35d292d75be284349d
rishabhyadavtech <rishabh.yadavtech1@gmail.com>
2026-07-24 09:54:25 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -471,7 +471,7 @@ async function submitReport() {
   body: JSON.stringify({
     message: message,
     report: reason,
-    customreason: customReason
+    custom_reason: customReason
   })
 });
 
