757f245967e60b332f62a7351a0b5bc5bb603148
rishabhyadavtech <rishabh.yadavtech1@gmail.com>
2026-07-31 13:51:52 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ async function checkScam() {
 
  const message = document.getElementById("input").value.trim();
 const textarea = document.getElementById("input");
-textarea.style.height = "100px";
+textarea.style.height = "90px";
 const language =
 document.getElementById("language").value;
   const resultBox = document.getElementById("result");
